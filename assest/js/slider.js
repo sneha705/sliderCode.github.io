324696f65fdf911c582a517b58a60a3eefb7287d
sneha705 <705sneha@gmail.com>
2019-08-29 14:54:16 +0530

    updated slide js

--- assest/js/slider.js
+++ assest/js/slider.js
@@ -1,21 +1,28 @@
 var obj = [
-    { "url": "assest/image/car1.jpg" },
-    { "url": "assest/image/car2.jpg" },
-    { "url": "assest/image/car3.jpg" },
-    { "url": "assest/image/car4.jpg" },
-    { "url": "assest/image/car5.jpg" },
-    { "url": "assest/image/bike1.jpg" },
-    { "url": "assest/image/bike2.jpg" },
-    { "url": "assest/image/bike3.jpg" },
-    { "url": "assest/image/bike4.jpg" },
-    { "url": "assest/image/bike5.jpg" },
-    { "url": "assest/image/cycle1.jpg" },
-    { "url": "assest/image/cycle2.jpg" },
-    { "url": "assest/image/cycle3.jpg" },
-    { "url": "assest/image/cycle4.jpg" },
-    { "url": "assest/image/cycle5.jpg" }
+    { "url": "assest/image/car1.jpg","type": "car"},
+    { "url": "assest/image/car2.jpg","type": "car" },
+    { "url": "assest/image/car3.jpg","type": "car"},
+    { "url": "assest/image/car4.jpg","type": "car" },
+    { "url": "assest/image/car5.jpg","type": "car" },
+    { "url": "assest/image/bike1.jpg","type": "bike"},
+    { "url": "assest/image/bike2.jpg","type": "bike" },
+    { "url": "assest/image/bike3.jpg","type": "bike" },
+    { "url": "assest/image/bike4.jpg","type": "bike" },
+    { "url": "assest/image/bike5.jpg","type": "bike" },
+    { "url": "assest/image/cycle1.jpg","type": "cycle"},
+    { "url": "assest/image/cycle2.jpg","type": "cycle" },
+    { "url": "assest/image/cycle3.jpg","type": "cycle" },
+    { "url": "assest/image/cycle4.jpg","type": "cycle" },
+    { "url": "assest/image/cycle5.jpg","type": "cycle" }
 ];
+//filter image obj
+  function imgFilter (imgObj,value) {
+    $("#sliderUl").css('margin-left','0px');
 
+     var img=imgObj.filter(({type})=>type===value);
+     return img;
+    }
+    
 //slider image create
 function imgArr(arr) {
     var div = document.getElementsByTagName('div');
@@ -27,9 +34,6 @@ function imgArr(arr) {
         div[8].append(img);
     }
 }
-//create image function call
-imgArr(obj);
-
 
 //slider of sliderImage
 function sliderImag1(ImgIndex) {
@@ -72,14 +76,16 @@ function menuBar(ar) {
 
 
 // thumbnail image call
-menuBar(obj)
+//menuBar(obj)
 
 //function current Div
+   function currentDiv(){
     var thumbNail=document.getElementsByClassName("x");
     $(thumbNail).click(function () {
      var i=   $( thumbNail ).index( this );
      sliderImag1(slideIndex = i+1);
     });
+}
     
 
 //thumnail image  call on Prev button
@@ -90,16 +96,21 @@ function thumbnailPrevBtn(){
 
 }
 
+    
 //thumnail image  call on Next button
 function thumbnailNextBtn(){
-    if ( $('#sliderUl').css('marginLeft') === '-2222px' ){
+    thumbnail=document.getElementsByClassName("x");
+    size=(thumbnail.length*202)-(5*202);
+    a=( $('#sliderUl').css('marginLeft'));
+    
+    if (parseInt(a) < (-size) ){
     }
     else{
-        $('#sliderUl').animate({'margin-left': '-=202px'}, 100);
+       $('#sliderUl').animate({'margin-left': '-=202px'}, 100);
     }
    }
 
-   //show thumbnail on slider
+   //show slider on thumbnail slider
 function sliderMove(ImgIndex) {
     sliderImag1(slideIndex += ImgIndex);
 }
@@ -111,7 +122,7 @@ function thumbnailScrollNext(){
     if(sliderImgArr.className="active,sliderImgArray"){
 
         currentImg=document.getElementsByClassName("active sliderImgArray");
-        //currentImg[0].style.border="thick solid orange";
+       
         var slidertAtr=($(currentImg[0]).attr('src'));
     }
     thubnailArr=document.getElementsByClassName("x");
@@ -139,6 +150,8 @@ function thumbnailScrollPrev(){
     thubnailArr=document.getElementsByClassName("x");
     for (var i = 0; i < thubnailArr.length; i++) { 
         if($(thubnailArr[i]).attr('src')==slidertAtr){
+            
+             
              $('#sliderUl').animate({'margin-left': '+=202px'}, 100);
         }
 
@@ -147,8 +160,6 @@ function thumbnailScrollPrev(){
     
 }
 
-slideIndex = 1;
-sliderImag1(slideIndex);
 
 //calling btn function for prev button
 $("#arroPrev").click(function () {
@@ -159,13 +170,26 @@ $("#arroPrev").click(function () {
     
 });
 
+
+
+
+
+
+
+
+
+
 //calling btn function for next button
 $("#arrowNext").click(function () {
     sliderMove(1);
   //  thumbnailScrollNext();
    
-        if ( $('#sliderUl').css('marginLeft') === '-2222px' ){
-        }
+            thumbnail=document.getElementsByClassName("x");
+            size=(thumbnail.length*202)-(5*202);
+            a=( $('#sliderUl').css('marginLeft'));
+            
+            if (parseInt(a) < (-size) ){
+            }
         else{
             thumbnailScrollNext();
         }
@@ -181,5 +205,62 @@ $("#thumnNext").click(function () {
 $("#thumbPrev").click(function () {
     thumbnailPrevBtn();
     sliderMove(-1);
+});
+function empty(){
+    $(".sliderImage").empty();
+    $("#sliderUl").empty();
+}
+
+//Header menubar function call
+//wrapper function of All
+function wraperAll(){
+   var  all=obj;
+    empty();
+    imgArr(all);
+    menuBar(all);
+    sliderImag1(slideIndex=1);
+    currentDiv();
+}
+// All click func call
+$("#all").click(function(){
+    wraperAll();
+});
+//Onload function call 
+window.onload = function() {
+    wraperAll();
+  };
+  
+
+// car click func call
+$("#carHref").click(function(){
+   
+    carArray=imgFilter(obj,"car"); 
+    empty();
+    imgArr(carArray);
+    menuBar(carArray);
+    sliderImag1(slideIndex=1);
+    currentDiv();
+
+});
+// All bike func call
+ $("#bikeHref").click(function(){
+    bikeArray=imgFilter(obj,"bike"); 
+    empty();
+    imgArr(bikeArray);
+    menuBar(bikeArray);
+    sliderImag1(slideIndex=1);
+    currentDiv();
+
+
+});
+// cycle click func call
+$("#cycleHref").click(function(){
+    cycleArray=imgFilter(obj,"cycle"); 
+    empty();
+    imgArr(cycleArray);
+    menuBar(cycleArray);
+    sliderImag1(slideIndex=1);
+    currentDiv();
+
 });
 
